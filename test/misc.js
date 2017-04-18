51167adb78c51b0ab9078d650e8739af49354284
Troy Coutu <Autre31415@gmail.com>
2017-04-17 20:17:33 -0400

    dump the flushCache

--- test/misc.js
+++ test/misc.js
@@ -190,7 +190,6 @@ describe('Misc', function() {
 
   it('should minify template with internal minifier (misc/templateToMinify.html)', function(done) {
     teddy.minify(true);
-    teddy.flushCache('misc/templateToMinify.html');
     assert.equal(teddy.render('misc/templateToMinify.html', model), '<!DOCTYPE html><html lang=\'en\'> <head> <meta charset=\'utf-8\'> <meta name=\'viewport\' content=\'width=device-width,initial-scale=1\'> <meta name=\'format-detection\' content=\'telephone=no\'> <title>Plain HTML</title> </head> <body> <main> <p>This template contains no teddy tags. Just HTML.</p> </main> </body></html>');
     teddy.minify(false);
     done();
