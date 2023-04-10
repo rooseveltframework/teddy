64cff8cdde3a1b5ac0a223a3ae44a4e1c1f715d4
Eric Newport <kethinov@gmail.com>
2023-04-10 12:26:56 -0400

    new test for #574
    this also breaks #574

--- test/looping.js
+++ test/looping.js
@@ -61,6 +61,11 @@ describe('Looping', function () {
     done()
   })
 
+  it.skip('should render {variables} (looping/varNameViaVarInLoopWithIndependentVars.html)', function (done) {
+    assert.equalIgnoreSpaces(teddy.render('looping/varNameViaVarInLoopWithIndependentVars.html', model), '<p>guy</p><p>girl</p><p>landscape</p>')
+    done()
+  })
+
   it.skip('should render {variables} (looping/commentedLoopInLoop.html)', function (done) {
     assert.equalIgnoreSpaces(teddy.render('looping/commentedLoopInLoop.html', model), '<p></p>')
     done()
