e0a279b9fb99ba5263ad9a8a8d298311611e1082
Railon Acosta <Cagedtreestechnology@gmail.com>
2023-05-08 09:27:16 -0400

    Remove `done` from all tests (#601)

--- test/includes.js
+++ test/includes.js
@@ -24,99 +24,80 @@ describe('Includes', function () {
     }
   })
 
-  it('should <include> a template (includes/include.html)', function (done) {
+  it('should <include> a template (includes/include.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/include.html', model), '<p>Some content</p>')
-    done()
   })
 
-  it('should <include> all templates (includes/includeMultipleTemplates.html)', function (done) {
+  it('should <include> all templates (includes/includeMultipleTemplates.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeMultipleTemplates.html', model), '<p>test test</p> <p>test test</p> <p>test test</p>')
-    done()
   })
 
-  it('should <include> a template whose name is populated by a {variable} (includes/dynamicInclude.html)', function (done) {
+  it('should <include> a template whose name is populated by a {variable} (includes/dynamicInclude.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/dynamicInclude.html', model), '<p>Some content</p>')
-    done()
   })
 
-  it('should populate <include> <arg> in the child template; the class should render (includes/includeArgCheckedByOneLineIfWrapper.html)', function (done) {
+  it('should populate <include> <arg> in the child template; the class should render (includes/includeArgCheckedByOneLineIfWrapper.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeArgCheckedByOneLineIfWrapper.html', model), '<p class="populated">Is it populated? populated</p>')
-    done()
   })
 
-  it('should <include> a template with arguments (includes/includeWithArguments.html)', function (done) {
+  it('should <include> a template with arguments (includes/includeWithArguments.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeWithArguments.html', model), '<p>override</p>')
-    done()
   })
 
-  it('should <include> a template with a nested include (includes/nestedInclude.html)', function (done) {
+  it('should <include> a template with a nested include (includes/nestedInclude.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/nestedInclude.html', model), '<div><p>Some content</p></div>')
-    done()
   })
 
-  it('should <include> a template with a nested include passing a text argument (includes/nestedIncludeWithArg.html)', function (done) {
+  it('should <include> a template with a nested include passing a text argument (includes/nestedIncludeWithArg.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/nestedIncludeWithArg.html', model), '<div><p>nested</p></div>')
-    done()
   })
 
-  it('should <include> a template with loop arguments (includes/nestedLoop.html)', function (done) {
+  it('should <include> a template with loop arguments (includes/nestedLoop.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/nestedLoop.html', model), '<p>a</p><p>b</p><p>c</p>')
-    done()
   })
 
-  it('should ignore and skip rendering orphaned argument (includes/orphanedArgument.html)', function (done) {
+  it('should ignore and skip rendering orphaned argument (includes/orphanedArgument.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/orphanedArgument.html', model), '<div></div>')
-    done()
   })
 
-  it('should <include> a template that contains loops and variables with an argument (includes/includeLoopsAndVars.html)', function (done) {
+  it('should <include> a template that contains loops and variables with an argument (includes/includeLoopsAndVars.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeLoopsAndVars.html', model), '<p>a</p><p>b</p><p>c</p><p>world</p><p>guy</p>')
-    done()
   })
 
-  it('should <include> a template that contains numerical {variables} (includes/numericVarInArg.html)', function (done) {
+  it('should <include> a template that contains numerical {variables} (includes/numericVarInArg.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/numericVarInArg.html', model), '<p>STRING!</p>')
-    done()
   })
 
-  it('should <include> a template with numeric arguments (includes/numericArgument.html)', function (done) {
+  it('should <include> a template with numeric arguments (includes/numericArgument.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/numericArgument.html', model), '<p>Hello!</p>')
-    done()
   })
 
-  it('should escape the contents of a script when included in a template (includes/inlineScriptTag.html)', function (done) {
+  it('should escape the contents of a script when included in a template (includes/inlineScriptTag.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/inlineScriptTag.html', model), '<p>Hello!</p><script>console.log(\'Hello world\'); for (var i = 0; i < 2; i++) { console.log(\'Test\') } </script>')
-    done()
   })
 
-  it('should evaluate {variable} outside of include as original model value (includes/argRedefineModelVar.html)', function (done) {
+  it('should evaluate {variable} outside of include as original model value (includes/argRedefineModelVar.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/argRedefineModelVar.html', model), '<style>p { height: 10px; }</style> <p>Some content</p>')
-    done()
   })
 
-  it('should prevent recursion abuse (includes/argVariableWithinArg.html)', function (done) {
+  it('should prevent recursion abuse (includes/argVariableWithinArg.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/argVariableWithinArg.html', model), '<p>Some content</p>')
-    done()
   })
 
-  it('should <include> a template and render pageContent inside of <if> (includes/includeIfContent.html)', function (done) {
+  it('should <include> a template and render pageContent inside of <if> (includes/includeIfContent.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeIfContent.html', model), '<p>hello</p>')
-    done()
   })
 
-  it('should <include> a template and render pageContent <arg> contents and correctly parse <if>, <loop>, and <if> tags (includes/includeComplexContent.html)', function (done) {
+  it('should <include> a template and render pageContent <arg> contents and correctly parse <if>, <loop>, and <if> tags (includes/includeComplexContent.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeComplexContent.html', model), '<section class="content"><article class="thing"><section class="blah">other_prop_one</section></article><article class="thing"><section class="blah">other_prop_two</section></article></section>')
-    done()
   })
 
-  it('should <include> a template and escape regex pattern in argument (includes/includeEscapeRegex.html)', function (done) {
+  it('should <include> a template and escape regex pattern in argument (includes/includeEscapeRegex.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeEscapeRegex.html', model), '<input type="text" name="date" placeholder="DD/MM/YYYY" id="date" pattern="^(3[0-1]|[1-2]\\d|[1-9]|0\\d)\\/(1[0-2]|[1-9]|0\\d)\\/[1-2]\\d{3}$">')
-    done()
   })
 
-  it('should ignore includes with invalid markup (includes/invalidIncludeMarkup.html)', function (done) {
+  it('should ignore includes with invalid markup (includes/invalidIncludeMarkup.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/invalidIncludeMarkup.html', model), '<div><p>Some content</p></div>')
-    done()
   })
 
   it('should escape from infinite loop of includes via setMaxPasses (includes/includeInfiniteLoop.html)', function (done) {
@@ -130,13 +111,11 @@ describe('Includes', function () {
     done()
   })
 
-  it('should evaluate a nested reverse quotes oneliner with an arg passed to it (includes/nestedOneliner.html)', function (done) {
+  it('should evaluate a nested reverse quotes oneliner with an arg passed to it (includes/nestedOneliner.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/nestedOneliner', model), '<p class="Some content">One line if.</p>')
-    done()
   })
 
-  it('should populate <include> <arg> in the child template (includes/includeArgCheckedByOneLineIfWrapper.html)', function (done) {
+  it('should populate <include> <arg> in the child template (includes/includeArgCheckedByOneLineIfWrapper.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('includes/includeArgCheckedByOneLineIfWrapper', model), '<p class="populated">Is it populated? populated</p>')
-    done()
   })
 })
--- test/misc.js
+++ test/misc.js
@@ -26,95 +26,78 @@ describe('Misc', function () {
     }
   })
 
-  it('should not escape HTML entities present in {variables} which are properly {flagged|p|s} (misc/barPandSTest.html)', function (done) {
+  it('should not escape HTML entities present in {variables} which are properly {flagged|p|s} (misc/barPandSTest.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/barPandSTest.html', model), '<h1>double bars</h1> {something}')
-    done()
   })
 
-  it('should not escape HTML entities present in {variables} which are properly {flagged|s|p} (misc/barSandPTest.html)', function (done) {
+  it('should not escape HTML entities present in {variables} which are properly {flagged|s|p} (misc/barSandPTest.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/barSandPTest.html', model), '<h1>double bars</h1> {something}')
-    done()
   })
 
-  it('should render {variables} (misc/variable.html)', function (done) {
+  it('should render {variables} (misc/variable.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/variable.html', model), '<p>Some content</p>')
-    done()
   })
 
-  it('should render multiple {variables} (misc/multipleVariables.html)', function (done) {
+  it('should render multiple {variables} (misc/multipleVariables.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/multipleVariables.html', model), '<p>Some content</p> <h5>More content</h5>')
-    done()
   })
 
-  it('should render nested {variables} (misc/nestedVars.html)', function (done) {
+  it('should render nested {variables} (misc/nestedVars.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/nestedVars.html', model), '<p>Variable with a variable inside: And another: Some content</p>')
-    done()
   })
 
-  it('should not render nested {variables|p} (misc/nestedVarsParseFlag.html)', function (done) {
+  it('should not render nested {variables|p} (misc/nestedVarsParseFlag.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/nestedVarsParseFlag.html', model), '<p>Variable with a variable inside: {subVar}</p>')
-    done()
   })
 
-  it('should properly escape HTML entities present in {variables} (misc/varEscaping.html)', function (done) {
+  it('should properly escape HTML entities present in {variables} (misc/varEscaping.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/varEscaping.html', model), '<p>&lt;span&gt;raw html&lt;/span&gt;</p>')
-    done()
   })
 
-  it('should not escape HTML entities present in {variables} which are properly {flagged|s} (misc/varNoEscaping.html)', function (done) {
+  it('should not escape HTML entities present in {variables} which are properly {flagged|s} (misc/varNoEscaping.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/varNoEscaping.html', model), '<div><span>raw html</span></div>')
-    done()
   })
 
-  it('should not parse any code in <noteddy> tags (misc/varNoParsing.html)', function (done) {
+  it('should not parse any code in <noteddy> tags (misc/varNoParsing.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/varNoParsing.html', model), '<p>{escapeTest}</p>')
-    done()
   })
 
-  it('should remove {! server side comments !} (misc/serverSideComments.html)', function (done) {
+  it('should remove {! server side comments !} (misc/serverSideComments.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/serverSideComments.html', model), '<p>test test</p>')
-    done()
   })
 
   // #27 and #43
-  it('should remove {! {! nested !} server side comments !} (misc/serverSideCommentsNested.html)', function (done) {
+  it('should remove {! {! nested !} server side comments !} (misc/serverSideCommentsNested.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/serverSideCommentsNested.html', model), '<p>Any comments? </p>')
-    done()
   })
 
-  it('should not break when referencing objects that don\'t exist (misc/objectDoesNotExist.html)', function (done) {
+  it('should not break when referencing objects that don\'t exist (misc/objectDoesNotExist.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/objectDoesNotExist.html', model), ' <p>{doesntExist.someKey}</p> <p class="false"></p>')
-    done()
   })
 
-  it('should render plain HTML with no teddy tags with no changes (misc/plainHTML.html)', function (done) {
+  it('should render plain HTML with no teddy tags with no changes (misc/plainHTML.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/plainHTML.html', model), '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><meta name="format-detection" content="telephone=no"><title>Plain HTML</title><link rel="stylesheet" href="/css/styles.css"></head><body><main><p>This template contains no teddy tags. Just HTML.</p></main><script type="text/javascript" src="/js/main.js"></script></body></html>')
-    done()
   })
 
-  it('should render {variables} within style element (misc/styleVariables.html)', function (done) {
+  it('should render {variables} within style element (misc/styleVariables.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/styleVariables.html', model), '<style>p{height:10px;}</style>')
-    done()
   })
 
-  it('should access property of {variable} object with {variable} (misc/variableObjectProperty.html)', function (done) {
+  it('should access property of {variable} object with {variable} (misc/variableObjectProperty.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/variableObjectProperty.html', model), '<p>guy</p>')
-    done()
   })
 
-  it('should escape curly braces from regex pattern (misc/regexEscaping.html)', function (done) {
+  it('should escape curly braces from regex pattern (misc/regexEscaping.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/regexEscaping.html', model), '<input type="text" name="date" placeholder="DD/MM/YYYY" id="date" pattern="^(3[0-1]|[1-2]\\d|[1-9]|0\\d)\\/(1[0-2]|[1-9]|0\\d)\\/[1-2]\\d{3}$">')
-    done()
   })
 
   // TODO: fix this test; it works in node tests but breaks karma tests for some reason
-  it.skip('should render emojis correctly (misc/emojis.html)', function (done) {
+  it.skip('should render emojis correctly (misc/emojis.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/emojis.html', model), '<p>🎉🥳🎈🎊</p>')
-    done()
   })
 
   // TODO: possibly remove this test, as the cacheRenders feature was removed in 0.6.x
-  it.skip('should trigger caching rollover given one template with 100 unique models (misc/variable.html)', function (done) {
+  it.skip('should trigger caching rollover given one template with 100 unique models (misc/variable.html)', function () {
     let i
     teddy.cacheRenders(true)
     teddy.setDefaultCaches(10)
@@ -125,11 +108,10 @@ describe('Misc', function () {
     assert.equalIgnoreSpaces(renderedTemplates['misc/variable.html'][0].renderedTemplate, '<p>90</p>')
     teddy.setDefaultCaches(1)
     teddy.cacheRenders(false)
-    done()
   })
 
   // TODO: possibly remove this test, as the cacheRenders feature was removed in 0.6.x
-  it.skip('should not cache a blacklisted template (misc/variable.html)', function (done) {
+  it.skip('should not cache a blacklisted template (misc/variable.html)', function () {
     teddy.cacheRenders(true)
     teddy.setRenderedTemplates({})
     teddy.setCacheBlacklist(['misc/variable.html'])
@@ -138,11 +120,10 @@ describe('Misc', function () {
     assert.strictEqual(renderedTemplates['misc/variable.html'], undefined)
     teddy.setCacheBlacklist([])
     teddy.cacheRenders(false)
-    done()
   })
 
   // TODO: possibly remove this test, as the cacheRenders feature was removed in 0.6.x
-  it.skip('should only cache whitelisted templates (misc/variable.html)', function (done) {
+  it.skip('should only cache whitelisted templates (misc/variable.html)', function () {
     teddy.cacheRenders(true)
     teddy.setRenderedTemplates({})
     teddy.setCacheWhitelist({ 'misc/variable.html': 1 })
@@ -153,11 +134,10 @@ describe('Misc', function () {
     assert.equalIgnoreSpaces(renderedTemplates['misc/variable.html'][0].renderedTemplate, '<p>1</p>')
     teddy.setCacheWhitelist({})
     teddy.cacheRenders(false)
-    done()
   })
 
   // TODO: possibly remove this test, as the cacheRenders feature was removed in 0.6.x
-  it.skip('should only cache the whitelisted template the specified number of times (misc/variable.html)', function (done) {
+  it.skip('should only cache the whitelisted template the specified number of times (misc/variable.html)', function () {
     let i
     teddy.cacheRenders(true)
     teddy.setRenderedTemplates({})
@@ -172,25 +152,21 @@ describe('Misc', function () {
     assert.equalIgnoreSpaces(renderedTemplates['misc/variable.html'][0].renderedTemplate, '<p>90</p>')
     teddy.setCacheWhitelist({})
     teddy.cacheRenders(false)
-    done()
   })
 
-  it('should avoid rendering templates that are not strings', function (done) {
+  it('should avoid rendering templates that are not strings', function () {
     assert.equalIgnoreSpaces(teddy.render(5, model), '')
-    done()
   })
 
-  it('should render a template with missing or invalid model (misc/emptyModelMarkup.html)', function (done) {
+  it('should render a template with missing or invalid model (misc/emptyModelMarkup.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/emptyModelMarkup.html', 1), '<div><p>Hello</p></div>')
-    done()
   })
 
-  it('should not render {variables} that don\'t exist in the model (misc/varNotInModel.html)', function (done) {
+  it('should not render {variables} that don\'t exist in the model (misc/varNotInModel.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/varNotInModel.html', model), '{noExist}')
-    done()
   })
 
-  it('should set each verbosity level', function (done) {
+  it('should set each verbosity level', function () {
     let verbosity = ''
     teddy.setVerbosity()
     verbosity += teddy.params.verbosity + ', '
@@ -218,58 +194,48 @@ describe('Misc', function () {
     } else {
       teddy.setVerbosity(0)
     }
-    done()
   })
 
   // TODO: possibly remove this test, as the internal minifier feature was removed in 0.6.x
-  it.skip('should minify template with internal minifier (misc/templateToMinify.html)', function (done) {
+  it.skip('should minify template with internal minifier (misc/templateToMinify.html)', function () {
     teddy.compileAtEveryRender(true)
     teddy.minify(true)
     assert.equalIgnoreSpaces(teddy.render('misc/templateToMinify.html', model), '<!DOCTYPE html><html lang=\'en\'> <head> <meta charset=\'utf-8\'> <meta name=\'viewport\' content=\'width=device-width,initial-scale=1\'> <meta name=\'format-detection\' content=\'telephone=no\'> <title>Plain HTML</title> </head> <body> <main> <p>This template contains no teddy tags. Just HTML.</p> </main> </body></html>')
     teddy.minify(false)
     teddy.compileAtEveryRender(false)
-    done()
   })
 
   // TODO: possibly remove this test, as the cacheRenders feature was removed in 0.6.x
-  it.skip('should avoid flushing cache of non strings', function (done) {
+  it.skip('should avoid flushing cache of non strings', function () {
     assert.equalIgnoreSpaces(teddy.flushCache(5), '')
-    done()
   })
 
-  it('should render undefined variables as text (misc/undefinedVar.html)', function (done) {
+  it('should render undefined variables as text (misc/undefinedVar.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/undefinedVar.html', model), '<p>{undefinedVar}</p><p>{definedParent.undefinedMember}</p>')
-    done()
   })
 
-  it('should prevent infinitely referencing variables (misc/varRefVar.html)', function (done) {
+  it('should prevent infinitely referencing variables (misc/varRefVar.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/varRefVar.html', model), '{foo}')
-    done()
   })
 
-  it('should render empty strings as is for variables that are empty strings (misc/emptyStringVariable.html)', function (done) {
+  it('should render empty strings as is for variables that are empty strings (misc/emptyStringVariable.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/emptyStringVariable.html', model), '<p></p><p></p>')
-    done()
   })
 
-  it('should render template with extraneous whitespace properly (misc/extraneousWhitespace.html)', function (done) {
+  it('should render template with extraneous whitespace properly (misc/extraneousWhitespace.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/extraneousWhitespace.html', model), '<p>a</p><p>Something exists</p><p>b</p><p>Something exists</p><p>c</p><p>Something exists</p>')
-    done()
   })
 
-  it('should render {variables} that resolve to true or false boolean literals as strings (misc/printBooleanLiteral.html)', function (done) {
+  it('should render {variables} that resolve to true or false boolean literals as strings (misc/printBooleanLiteral.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/printBooleanLiteral.html', model), '<p>true</p><p>{somethingFalse}</p>')
-    done()
   })
 
-  it('should render {zero} as 0 (misc/zero.html)', function (done) {
+  it('should render {zero} as 0 (misc/zero.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/zero.html', model), '<p>0</p>')
-    done()
   })
 
-  it('should not render Teddy code in server-side comments in loops (misc/serverSideCommentsWithTeddyCode.html)', function (done) {
+  it('should not render Teddy code in server-side comments in loops (misc/serverSideCommentsWithTeddyCode.html)', function () {
     assert.equalIgnoreSpaces(teddy.render('misc/serverSideCommentsWithTeddyCode.html', model), '<div><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p><p>test</p></div>')
-    done()
   })
 
   if (typeof process === 'object') {
